ccbf3ccf6929ca9ccc2fb172fe20d5f4edd28400
ludongdong <1294694985@qq.com>
2016-05-12 17:56:04 +0800

    Commit a File

--- app/src/main/java/com/example/administrator/sharkoffdemo/appconfig/adsa.java
+++ app/src/main/java/com/example/administrator/sharkoffdemo/appconfig/adsa.java
@@ -1,8 +1,12 @@
 package com.example.administrator.sharkoffdemo.appconfig;
 
+import android.provider.ContactsContract;
+
+import static android.provider.ContactsContract.*;
+
 /**
  * Created by 卢冬冬 on 2016/5/9 0009.
  */
 public class adsa {
-
+//这是新的类
 }
